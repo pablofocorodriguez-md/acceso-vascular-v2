8adba0de926bd9695fa0c082348cdb2e200ca892
pablofocorodriguez-md <pablo.foco.rodriguez@gmail.com>
2026-04-01 16:05:53 -0300

    feat: agregar módulo pediátrico con motor de decisión separado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Adulto from './pages/Adulto';
+import Pediatrico from './pages/Pediatrico';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/adulto" element={<Adulto />} />
+        <Route path="/pediatrico" element={<Pediatrico />} />
       </Routes>
     </BrowserRouter>
   );
